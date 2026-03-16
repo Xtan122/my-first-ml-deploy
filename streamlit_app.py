e1106d7d792388b55ee9f2588e7e717e2d43d70d
Xtan122 <tn221495@gmail.com>
2026-03-16 20:01:05 +0700

    change the file name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,25 @@
 import streamlit as st
 import joblib
 
+# Load model và scaler
 model = joblib.load('linear_regression_model.pkl')
 scaler = joblib.load('scaler.pkl')
 
 # Streamlit app
-st.tittle('Student Test Score Prediction')
+st.title('Student Test Score Prediction')
 st.write('Enter the number of hours studied to predict the test score.')
 
 # User input
 hours = st.number_input('Hours Studied', min_value=0.0, step=1.0)
 
+# Predict
 if st.button('Predict'):
     try:
         data = [[hours]]
         scaled_data = scaler.transform(data)
         prediction = model.predict(scaled_data)
-        st.write(f"Predicted Test Score: {prediction[0]:.2f}")
+
+        st.success(f"Predicted Test Score: {prediction[0]:.2f}")
 
     except Exception as e:
         st.error(f'Error: {e}')
